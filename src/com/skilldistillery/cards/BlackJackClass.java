89220a6551122b6f51b0fad2e1ff4eb520ada8e4
roguetycoon <michaelmurrell49@gmail.com>
2018-11-18 20:12:13 -0700

    Finished Loop

--- src/com/skilldistillery/cards/BlackJackClass.java
+++ src/com/skilldistillery/cards/BlackJackClass.java
@@ -8,7 +8,7 @@ public class BlackJackClass {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		Deck deck = new Deck();
 		deck.makeDeck();
 		deck.shuffle();
@@ -17,24 +17,55 @@ public class BlackJackClass {
 		int money = 500;
 		System.out.println("What is  your name?");
 		String name = input.nextLine();
-		
+
 		System.out.println("Hi, " + name + "Let's play some BlackJack. \nYou have $500 to bet");
 //		System.out.println(deck);
 //		System.out.println(player);
 //		System.out.println(dealer);
 		boolean proceed = true;
-		
-		
-		
-		
-		
-		/*need while loop for most of game
-		 * logic for empty deck or deck running low
-		 * dealer holds at 17
-		 * dealing one blind card for dealer
+
+		player.add(deck.dealCard());
+		player.add(deck.dealCard());
+		dealer.add(deck.dealCard());
+		dealer.add(deck.dealCard());
+		System.out.println("Your hand is:");
+		System.out.println(player.toString());
+		System.out.println("For a total of:");
+		System.out.println(getValue(player));
+		System.out.println("Dealer has " + dealer.get(0).toString());
+		System.out.println("Woud you like to Hit(1) or Stand(2)?");
+		int choice = input.nextInt();
+//		System.out.println(dealer.toString());
+		if (choice == 1) {
+			player.add(deck.dealCard());
+			if (getValue(player)> 21 ) {
+				System.out.println("You busted with a hand of " + player.toString());
+			}
+			else if (getValue(player) == 21) {
+				System.out.println(" You have twenty one!!");
+				System.out.println(player.toString());
+			}
+			else {
+			System.out.println("Your hand is:");
+			System.out.println(player.toString());
+			System.out.println("For a total of:");
+			System.out.println(getValue(player));
+			}
+		}
+
+		/*
+		 * need while loop for most of game logic for empty deck or deck running low
+		 * dealer holds at 17 dealing one blind card for dealer
 		 */
-		
-		
+
 	}
 
+	public static int getValue(List<Card> hand) {
+		int totalValue = 0;
+		for (Card card : hand) {
+			totalValue += card.getValue();
+
+		}
+		return totalValue;
+	}
 }
--- src/com/skilldistillery/cards/Deck.java
+++ src/com/skilldistillery/cards/Deck.java
@@ -10,28 +10,33 @@ public class Deck {
 	public Deck() {
 		super();
 		makeDeck();
-		
+
 	}
+	
+
+	
+
 
 	public List<Card> makeDeck() {
 		Suit[] Suits = Suit.values();
-		Rank [] Ranks  = Rank.values();
+		Rank[] Ranks = Rank.values();
 		for (int i = 0; i < Suits.length; i++) {
 			for (int j = 0; j < Ranks.length; j++) {
-				Card card = new Card(Suits[i] , Ranks[j]);
+				Card card = new Card(Suits[i], Ranks[j]);
 				deck.add(card);
-				
+
 			}
 
 		}
 		return deck;
 	}
+	
 	@Override
 	public String toString() {
 		String deckList = "";
 		for (Card card : deck) {
 			deckList += "\n" + "    " + card.toString();
-			
+
 		}
 		return deckList;
 	}
@@ -39,19 +44,38 @@ public class Deck {
 	public int checkDeckSize() {
 		return deck.size();
 	}
-	
-	public void removeCard(Deck cards) {
+
+	public void removeCard(int i) {
 		Card card = deck.get(0);
 	}
-	public  Card dealCard() {
+
+	public Card dealCard() {
 		return deck.remove(0);
 	}
+
 	public Card getCard() {
 		return deck.get(0);
 	}
+
 	public void shuffle() {
 		Collections.shuffle(deck);
 	}
-	
+
+	public int printHandAndValue1(List<Card> hand, int value) {
+		for (Card card : hand) {
+			System.out.println(card);
+		}
+		System.out.println("Total value: " + value);
+		return value;
+	}
+
+	public int getValue(List<Card> hand) {
+		int totalValue = 0;
+		for (Card card : hand) {
+			totalValue += card.getValue();
+
+		}
+		return totalValue;
+	}
 
 }
